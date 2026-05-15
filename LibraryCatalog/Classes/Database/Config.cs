09ea9337cd33b8b3d9f871b0afa4987bf165525c
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 16:57:35 +0500

    Настройка подключения бд. Белослудцева

---
+++ LibraryCatalog/Classes/Database/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCatalog.Classes.Database
+{
+    public class Config
+    {
+        public static string connection = "server=localhost; uid=root; pwd=; database=LibraryCatalog;";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 30));
+    }
+}
