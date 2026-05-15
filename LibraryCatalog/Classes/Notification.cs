eb6a2d359dd5d64f9f6d240d456f9b428e00f58b
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 16:49:36 +0500

    Реализация INotifyPropertyChanged. Белослудцева

--- LibraryCatalog/Classes/Notification.cs
+++ LibraryCatalog/Classes/Notification.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace LibraryCatalog.Classes
 {
-    public class Notification 
+    public class Notification : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        protected virtual void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
