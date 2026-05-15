5c0ef087315bdc32d55dba3791b01b695e6f8090
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-15 17:00:50 +0500

    Разработанный контекст. Белослудцева

---
+++ LibraryCatalog/Context/LibraryContext.cs
@@ -0,0 +1,29 @@
+﻿using LibraryCatalog.Classes.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCatalog.Context
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public LibraryContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Config.Connection, Config.Version);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasKey(b => b.Id);
+        }
+    }
+}
